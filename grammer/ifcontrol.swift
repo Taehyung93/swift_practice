5e8f1f280a20847d3cfe984d8ccf9ad0471aacfc
Taehyung Kim <kaustris@gmail.com>
2020-05-14 15:54:26 +0900

    Create ifcontrol.swift

---
+++ grammer/ifcontrol.swift
@@ -0,0 +1,24 @@
+import UIKit
+
+var myAge = 32
+
+if myAge < 30 && myAge > 10 {
+    print(myAge)
+}
+else if myAge == 32 {
+    print("Shit.. you are age is ",myAge)
+}
+else {
+    print(myAge, "is so much to marry Taehyung")
+}
+//<, >, ==, !=, <=, >=
+//AND is && , OR is ||
+
+var myString = "James"
+
+//Instructer says James == james is true but I am not. I don't know why.
+if myString == "James" {
+    print("Yes I am")
+}
+
+
